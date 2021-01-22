b8cfa68066943a71d8a4aab07c466b52d438ccdd
Shea Mullaney <seagda@gmail.com>
2021-01-22 06:15:11 -0800

    Department done, UI updated, db reseeded

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 
 // connection data
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "employees_db"
-});
+const connection = mysql.createConnection({host: "localhost", port: 3306, user: "root", password: "password", database: "employees_db"});
 
 // start program after connection
 connection.connect(function(err) {
@@ -34,32 +28,19 @@ const mainMenu = () => {
         name: "whatToDo",
         type: "list",
         message: "MAIN MENU:",
-        choices: [
-            "Manage Departments", 
-            "Manage Roles", 
-            "Manage Employees", 
-            "Quit Application"
-            ]
-        
+        choices: ["Manage Departments", "Manage Roles", "Manage Employees", "Quit Application"]
         }).then(({ whatToDo }) => {
             switch (whatToDo) {
                 case "Manage Departments":
                     manageDepts();
                     break;
-    
                 case "Manage Roles":
                     manageRoles();
                     break;
-    
                 case "Manage Employees":
                     manageEmp();
                     break;
-
                 case "Quit Application":
-                    quitApp();
-                    break;
-                
-                default:
                     quitApp();
                     break;
         }
@@ -72,7 +53,7 @@ const quitApp = () => {
     console.log("-".repeat(50)+"\n Thanks for using Employee Manager. Goodbye.\n"+"-".repeat(50)+"\n");
 };
 
-// Manage Departments
+// Manage Department Functions Start Here ==================
 const manageDepts = () => {
     console.log("-".repeat(30)+"\n Now managing Departments:\n"+"-".repeat(30));
     connection.query("SELECT * FROM ??","department", function(err, data) {
@@ -83,11 +64,7 @@ const manageDepts = () => {
             name: "actionDept",
             type: "list",
             message: "What next?",
-            choices: ["Add Department", 
-                "Edit Department",
-                "Delete Department",
-                "Return to Main",
-                "Quit"]
+            choices: ["Add Department", "Edit Department", "Delete Department", "Return to Main", "Quit"]
             })
             .then(function(answer) {
                 switch (answer.actionDept) {
@@ -112,6 +89,10 @@ const manageDepts = () => {
                 case "Quit App":
                     quitApp();
                     break;
+
+                default:
+                    quitApp();
+                    break;
                 }
             });
         
@@ -121,16 +102,10 @@ const manageDepts = () => {
 // Add a Dept
 const addDept = () => {
     inquirer.prompt([
-        {
-         name: "newDept",
-         type: "input",
-         message: "Name of new department:"
-        }
+      {name: "newDept", type: "input", message: "Name of new department:"}
     ])
     .then(function(answer) {
-    connection.query("INSERT INTO department SET ?",
-    {name: answer.newDept},
-    function(err) {
+    connection.query("INSERT INTO department SET ?", {name: answer.newDept}, function(err) {
         if (err) throw err;
         console.log(`${answer.newDept} Department added.`);
         manageDepts();
@@ -143,23 +118,16 @@ const editDept = () => {
     connection.query("SELECT * FROM ??","department", function(err, deptData) {
         if (err) throw err;
         inquirer.prompt([
-            {
-             name: "deptChoice",
+            {name: "deptChoice",
              type: "rawlist",
              choices: function() {
                  let deptArray = [];
                  for (let i = 0; i < deptData.length; i++) {
                      deptArray.push(deptData[i].name);
                  }
-                 return deptArray;
-             },
-             message: "What department would you like to edit?"
-            },
-            {
-                name: "nameEdit",
-                type: "input",
-                message: "Enter new name:"
-            }
+                 return deptArray;},
+             message: "What department would you like to edit?"},
+            {name: "nameEdit", type: "input", message: "Enter new name:"}
         ]).then(function(answer) {
             let chosenDept;
             for (let i=0; i < deptData.length; i++) {
@@ -177,14 +145,252 @@ const editDept = () => {
 };
 
 // Delete a Dept
-const deleteDept = () => {};
+const deleteDept = () => {
+    connection.query("SELECT * FROM ??","department", function(err, deptData) {
+        if (err) throw err;
+        inquirer.prompt([
+            {name: "deptChoice",
+             type: "rawlist",
+             choices: function() {
+                 let deptArray = [];
+                 for (let i = 0; i < deptData.length; i++) {
+                     deptArray.push(deptData[i].name);
+                 }
+                 return deptArray;},
+             message: "What department would you like to delete?"}
+        ]).then(function(answer) {
+            let chosenDept;
+            for (let i=0; i < deptData.length; i++) {
+                if (deptData[i].name === answer.deptChoice){
+                    chosenDept = deptData[i];
+                }
+            }
+            connection.query("SELECT * FROM ?? WHERE department_id = ?", ["role", chosenDept.id], function(err, data) {
+                if (err){
+                 throw err;
+                } else if (!data.length){
+                  console.log("No associated roles, OK to delete...");
+                  (function deleteDept() {
+                    connection.query("DELETE FROM ?? WHERE ?", ["department", {id: chosenDept.id}], 
+                    function(err, res){
+                        if (err) throw err;
+                        console.log("\n\n** Department successfully deleted.**\nHit 'Enter' to Manage Departments");
+                    })
+                    
+                  })();
+                } else {
+                console.log("The following Roles are associated with this Department. Cannot delete.")
+                console.table(data);
+                }
+                mainMenu();
+            })
+        });
+    });
+};
+// ============ END MANAGE DEPTS =================
 
-// Manage Roles
+// Manage Role Functions Start Here ==============
 const manageRoles = () => {
     console.log("-".repeat(30)+"\n Now managing Roles:\n"+"-".repeat(30));
+    connection.query("SELECT role.title, role.salary, department.name AS department FROM ?? LEFT JOIN ?? ON role.department_id = department.id",
+    ["role","department"],
+     function(err, data) {
+        if (err) throw err;
+        console.table(data);
+        console.log("_".repeat(30));
+        inquirer.prompt({
+            name: "actionRole",
+            type: "list",
+            message: "What next?",
+            choices: ["Add Role", "Edit Role", "Delete Role", "Return to Main", "Quit"]
+            })
+            .then(function(answer) {
+            switch (answer.actionRole) {
+
+            case "Add Role":
+                addRole();
+                break;
+            case "Edit Role":
+                editRole();
+                break;
+            case "Delete Role":
+                deleteRole();
+                break;
+            case "Return to Main":
+                console.log("=".repeat(30));
+                mainMenu();
+                break;                
+            case "Quit App":
+                quitApp();
+                break;
+            default:
+                quitApp();
+                break;
+            }
+        });      
+    }) 
 };
 
-// Manage Employees
+// Add a Role
+const addRole = () => {
+    inquirer.prompt([
+      {name: "newRole", type: "input", message: "Name of new role:"},
+      {name: "newSalary", type: "input", message: "Salary for this role:"},
+      {
+        name: "newDeptId", 
+        type: "rawlist", 
+        choices: function() {
+            connection.query("SELECT * FROM ??","department", function(err, deptData) {
+                if (err) throw err;
+                let deptArray = [];
+                for (let i = 0; i < deptData.length; i++) {
+                deptArray.push(deptData[i].name);
+                }
+                return deptArray;
+            });
+        },
+        message: "Choose a Department:"
+    }
+    ])
+    .then(function(answer) {
+    connection.query("INSERT INTO role SET ?,?,?", [
+        {name: answer.newRole},
+        {salary: answer.newSalary},
+        {department_id: answer.newDeptId}
+    ], function(err) {
+        if (err) throw err;
+        console.log(`${answer.newRole} added.`);
+        manageRoles();
+     });
+  })
+};
+
+// Edit a Role
+const editRole = () => {
+    connection.query("SELECT * FROM ??","role", function(err, roleData) {
+        if (err) throw err;
+        inquirer.prompt([
+            {name: "roleChoice",
+             type: "rawlist",
+             choices: function() {
+                 let roleArray = [];
+                 for (let i = 0; i < roleData.length; i++) {
+                     roleArray.push(roleData[i].name);
+                 }
+                 return roleArray;},
+             message: "What Role would you like to edit?"},
+            {name: "nameEdit", type: "input", message: "Enter new name:"}
+        ]).then(function(answer) {
+            let chosenRole;
+            for (let i=0; i < roleData.length; i++) {
+                if (roleData[i].name === answer.roleChoice){
+                    chosenRole = roleData[i];
+                }
+            }
+            connection.query("UPDATE ?? SET ? WHERE ?",["role",{name:answer.nameEdit},{id:chosenRole.id}], function(err) {
+                if (err) throw err;
+                console.log("Role updated.");
+                manageRoles();
+            })
+        });
+    });
+};
+
+// Delete a Role
+const deleteRole = () => {
+    connection.query("SELECT * FROM ??", "role", function(err, roleData) {
+        if (err) throw err;
+        inquirer.prompt([
+            {name: "roleChoice",
+             type: "rawlist",
+             choices: function() {
+                 let roleArray = [];
+                 for (let i = 0; i < roleData.length; i++) {
+                     roleArray.push(roleData[i].name);
+                 }
+                 return roleArray;},
+             message: "Which Role would you like to delete?"}
+        ]).then(function(answer) {
+            let chosenRole;
+            for (let i=0; i < roleData.length; i++) {
+                if (roleData[i].name === answer.roleChoice){
+                    chosenRole = roleData[i];
+                }
+            }
+            connection.query("SELECT * FROM ?? WHERE role_id = ?", ["employee", chosenRole.id], function(err, data) {
+                if (err){
+                 throw err;
+                } else if (!data.length){
+                  console.log("No associated employees, OK to delete...");
+                  (function deleteRole() {
+                    connection.query("DELETE FROM ?? WHERE ?", ["role", {id: chosenRole.id}], 
+                    function(err, res){
+                        if (err) throw err;
+                        console.log("\n\n** Role successfully deleted.**\nHit 'Enter' to Manage more Roles");
+                    })
+                    
+                  })();
+                } else {
+                console.log("The following Roles are associated with this Department. Cannot delete.")
+                console.table(data);
+                }
+                mainMenu();
+            })
+        });
+    });
+};
+
+// ============ END MANAGE ROLES =================
+
+// Manage Employee Functions Start Here ==============
 const manageEmp = () => {
     console.log("-".repeat(30)+"\n Now managing Employees:\n"+"-".repeat(30));
+    connection.query("SELECT e.first_name, e.last_name, r.title, d.name AS department FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id",
+     function(err, data) {
+        if (err) throw err;
+        console.table(data);
+        console.log("_".repeat(30));
+        inquirer.prompt({
+            name: "actionEmp",
+            type: "list",
+            message: "What next?",
+            choices: ["Add Employee", "Update Employee", "Terminate Employee", "Return to Main", "Quit"]
+            })
+            .then(function(answer) {
+            switch (answer.actionEmp) {
+
+            case "Add Employee":
+                addEmp();
+                break;
+            case "Update Employee":
+                updateEmp();
+                break;
+            case "Terminate Employee":
+                termEmp();
+                break;
+            case "Return to Main":
+                console.log("=".repeat(30));
+                mainMenu();
+                break;                
+            case "Quit App":
+                quitApp();
+                break;
+            default:
+                quitApp();
+                break;
+            }
+        });      
+    }) 
+};    
+
+const addEmp = () => {
+
+};
+
+const updateEmp = () => {
+
+};
+
+const termEmp = () => {
+
 };
--- seed_employees.sql
+++ seed_employees.sql
@@ -1,17 +1,37 @@
 USE employees_db;
 
 INSERT INTO department (name) VALUES
-    ("Accounting"),("HR"),("Engineering"),("Sales"),("Executive");
+    ("Executive"),
+    ("Finance"),
+    ("HR"),
+    ("Engineering"),
+    ("Sales"),
+    ("Operations");
 
 INSERT INTO role (title,salary,department_id) VALUES
-    ("Accounts Receivable",60000,1),
-    ("Accounts Payable",40000,1),
-    ("Sales Manager",70000,4),
-    ("Payroll Supervisor", 56000,2),
-    ("Data Engineer", 87000, 3),
-    ("Web Developer", 95000, 3),
-    ("CEO",1000000,5);
+    ("CEO", 900000, 1),
+    ("COO", 495000, 1),
+    ("CFO", 495000, 1),
+    ("CTO", 600000, 1);
+    ("HR Director",100000,3),
+    ("Sales Director",200000,5),
+    ("Office Manager",65000,6)
+    ("Facilities Manager",70000,6),
+    ("Accounts Receivable",60000,2),
+    ("Accounts Payable",40000,2),
+    ("Payroll Supervisor", 56000, 2),
+    ("Salesperson", 70000, 5)
+    ("Data Engineer", 87000, 4),
+    ("Web Developer", 95000, 4);
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES
-    ("Joe","Smith",3,NULL),
-    ("Shea","Mullaney", 5, NULL);
+    ("Betty","Badass", 1, NULL),
+    ("Joseph","Fixit", 2, 1),
+    ("Finance","Guru", 3, 1),
+    ("Shea","Mullaney", 4, 1),    
+    ("Cara","Smith", 5, 2),
+    ("Number", "Cruncher", 9, 3),
+    ("Dora", "Explorer", 7, 3),
+    ("Midas","Touch", 5, 3),
+    ("Alan","Donovan", 12, 3),
+    ("Louie","Luau", 14, 4);
